534d0814f57a02f576ed66ce185df1190824cc44
zakharovda95 <zakharovda95@gmail.com>
2022-08-21 23:53:33 +0300

    stack list icons

---
+++ components/shared/ContentGenerator.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="content-generator"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
--- helpers/content/stack/stack-list.content.ts
+++ helpers/content/stack/stack-list.content.ts
@@ -2,23 +2,23 @@ import { StackListItemType } from '~/helpers/types/content/stack-list-item.types
 
 export const STACK_LIST_RUS: StackListItemType[] = [
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/html.svg',
     name: 'HTML 5',
     description: 'Основные теги, семантическая верстка',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/css.svg',
     name: 'CSS 3',
     description: 'Основные правила, позиционирование, Flexbox, Grid, префиксы, медиазапросы',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/js.svg',
     name: 'JavaScript',
     description:
       'Синтаксис ES 6+, ООП (классы, основы наследования), Ассинхронность (события, таймауты, промисы, async/await), REST API (Fetch, Axios, HTTP), ES-модули, обработка ошибок, работа с датой и временем, коллекции данных',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/ts.svg',
     name: 'TypeScript',
     description: 'Типы, enum, интерфейсы, наследование типов',
   },
@@ -29,89 +29,89 @@ export const STACK_LIST_RUS: StackListItemType[] = [
       'Vue 2/Vue3 (Option/Composition API), SFC, SPA, компоненты и их взаимодействие, Стейт-менеджмент (Vuex, Pinia) Роутинг (Vue-Router), хуки, слоты, основы юнит-тестирования (test-utils, testing-library), Валидация (VeeValidate), PWA (Vue PWA)',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/nuxt.svg',
     name: 'NuxtJS',
     description: 'Nuxt 2/Nuxt 3, middlewares, fetch, asyncData',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/mdl.svg',
     name: 'MDLite',
     description: 'Подключение, работа с библиотекой',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/naive.svg',
     name: 'NaiveUI',
     description: 'Подключение, работа с библиотекой',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/sass.svg',
     name: 'SASS',
     description: 'Синтаксис SCSS, вложенности, переменные, условия',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/moment.svg',
     name: 'Moment',
     description: 'Основные методы для работы с датой и временем',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/lodash.svg',
     name: 'Lodash',
     description: 'Основные методы для работы с коллекциями данных',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/axios.svg',
     name: 'Axios',
     description: 'Создание экземпляров, заголовки, параметры',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/parcel.svg',
     name: 'Parcel-Bundler',
     description: 'Подключение, развертывание среды разработки, билд проекта',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/eslint.svg',
     name: 'ESLint',
     description: 'Оформление .eslintrc, подключение расширений, расстановка приоритетов',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/prettier.svg',
     name: 'Prettier',
     description: 'Оформление .prettierrc, основные правила, сихнхронизация с ESLint',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/github.svg',
     name: 'GitHub',
     description:
       'Создание репозитория, клонирование, установка зависимостей, создание/мерж веток, Push/Pull изменений, создание Pull Request, GitHub Pages',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/firebase.svg',
     name: 'Firebase',
     description: 'Подключение, авторизация/аутентификация, база данных, хранилище, хостинг',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/strapi.svg',
     name: 'StrapiCMS',
     description:
       'Подключение к проекту, создание контента/моделей/ролей/relations/API, несложная кастомизация бекенда',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/postman.svg',
     name: 'Postman',
     description: 'Создание запрсов, оформление библиотеки API',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/swagger.svg',
     name: 'SwaggerUI',
     description: 'Необходимый минимум для фронтендера: авторизация юзера, выполнение запросов',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/lerna.svg',
     name: 'LernaJS',
     description: 'Создание монорепозитория, определение проекта как зависимости, версионинг',
   },
   {
-    icon: '/assets/img/stack-icons/vue.svg',
+    icon: '/assets/img/stack-icons/figma.svg',
     name: 'Figma',
     description: 'Основные функции редактора',
   },
