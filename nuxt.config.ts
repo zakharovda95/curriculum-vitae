b82db7792fbcfb482ddf834aabf758f1c479e079
zakharovda95 <zakharovda95@gmail.com>
2022-08-30 20:56:03 +0400

    add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,5 @@ import { defineNuxtConfig } from 'nuxt';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
+  modules: ['@pinia/nuxt'],
 });
---
+++ stores/summary-page.store.ts
@@ -0,0 +1,3 @@
+import { defineStore } from 'pinia';
+
+export const useSummaryPageStore = defineStore('summaryPageStore', {});
